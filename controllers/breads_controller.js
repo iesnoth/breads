f09830d1dd199f0e5b3a62dfc59a1c0d1dbc6cf8
Victoria Beasley <iesnoth91@gmail.com>
2022-10-19 18:45:25 -0500

    adding data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,14 +1,18 @@
 const express = require('express')
-const breads =  express.Router()
+const breads = express.Router()
 const Bread = require(`../models/bread.js`)
 
 //INDEX
-breads.get(`/`,(req, res) =>{
-    res.send(Bread)
+breads.get(`/`, (req, res) => {
+    res.render('Index',
+        {
+            breads: Bread
+        }
+    )
 })
 
 //SHOW
-breads.get(`/:arrayIndex`,(req,res) => {
+breads.get(`/:arrayIndex`, (req, res) => {
     res.send(Bread[req.params.arrayIndex])
 })
 
---
+++ views/index.jsx
@@ -0,0 +1,20 @@
+const React = require(`react`)
+const Default = require(`./layouts/default`)
+
+function Index({breads}) {
+    return (
+        <Default>
+            <h2>Index Page</h2>
+            {/* <p>I have {breads[0].name} bread!</p> */}
+            <ul>
+                {
+                  breads.map((bread,index) =>{
+                    return(<li key={index}></li>)
+                  })
+                }
+            </ul>
+        </Default>
+    )
+}
+
+module.exports = Index
