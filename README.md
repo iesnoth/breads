fa545fc0b1a0454578c6dc17987443971868e610
Victoria Beasley <iesnoth91@gmail.com>
2022-10-29 09:10:22 -0500

    mongoose

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Express full CRUD with SSR JSX
 
 ## To Do
 - make a glossary of terms for variables and names
+- make cute bread art
 
 ### Things I learned
 - Only forms can make requests that are not GET
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-require('dotenv').config()
 //DEPENDENCIES
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 //CONFIGURATION
 const PORT = process.env.PORT
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )  
 const app = express()
 const methodOverride = require('method-override')
 
